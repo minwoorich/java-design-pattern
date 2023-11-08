88f3784e80cfc347ebb15f389f21e57bfa1f8f90
Minwoo Jhung <minwoorich@naver.com>
2023-11-08 21:06:23 +0900

    추상팩토리 패턴

--- src/main/java/abstractfactory/팩토리메서드사용/Client.java
+++
@@ -1,7 +0,0 @@
-package abstractfactory.팩토리메서드사용;
-
-public class Client {
-    public static void main(String[] args) {
-        Car car = new CarFactory(new Wheel(), new Handle()).create();
-    }
-}
